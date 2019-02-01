db9552ebcfeffe4845d766ed975416125ca51250
enikeishik <enikeishik@gmail.com>
2019-02-01 13:04:37 +0300

    fix: move common part out from condition

--- resources/templates/index.php
+++ resources/templates/index.php
@@ -12,13 +12,13 @@
 <title><?=$section->title?></title>
 </head>
 <body>
+<h1><?=$section->title?></h1>
+<p>
+    <a href="<?=$section->path?>">All</a> | 
+    <a href="<?=$section->path?>/marked">marked</a> | 
+    <a href="<?=$section->path?>/unmarked">unmarked</a>
+</p>
 <?php if (!empty($items) && is_array($items) && count($items) > 0): ?>
-    <h1><?=$section->title?></h1>
-    <p>
-        <a href="<?=$section->path?>">All</a> | 
-        <a href="<?=$section->path?>/marked">marked</a> | 
-        <a href="<?=$section->path?>/unmarked">unmarked</a>
-    </p>
     <?php foreach ($items as $item): ?>
         <p>
         <b><?=$item['title']?></b><br>
