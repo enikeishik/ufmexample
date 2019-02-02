e6799388c32429e67e6faa23f7acc9674132d427
enikeishik <enikeishik@gmail.com>
2019-02-02 11:10:10 +0300

    feat: module widget

---
+++ src/WidgetController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * UFO Framework Example module.
+ * 
+ * @copyright   Copyright (C) 2018 - 2019 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Ufo\Modules\Enikeishik\Ufmexample;
+
+use Ufo\Core\Result;
+use Ufo\Core\Section;
+use Ufo\Modules\Controller as BaseController;
+use Ufo\Modules\ModelInterface;
+use Ufo\Modules\View;
+use Ufo\Modules\ViewInterface;
+
+class WidgetController extends BaseController
+{
+    /**
+     * @see parent
+     */
+    public function compose(Section $section = null): Result
+    {
+        $this->setData($section);
+        
+        return new Result($this->getView());
+    }
+    
+    /**
+     * @see parent
+     */
+    protected function setDataFromModel(ModelInterface $model): void
+    {
+        $this->data['items'] = $model->getItems();
+    }
+    
+    /**
+     * @see parent
+     */
+    protected function getModelObject(): ModelInterface
+    {
+        return new WidgetModel();
+    }
+    
+    /**
+     * @see parent
+     */
+    protected function getViewObject(): ViewInterface
+    {
+        return new View('widget');
+    }
+}
---
+++ src/WidgetModel.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * UFO Framework Example module.
+ * 
+ * @copyright   Copyright (C) 2018 - 2019 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Ufo\Modules\Enikeishik\Ufmexample;
+
+use Ufo\Modules\Model as BaseModel;
+
+/**
+ * Module level model base class.
+ */
+class WidgetModel extends BaseModel
+{
+    /**
+     * Get items data.
+     * @return array
+     */
+    public function getItems(): array
+    {
+        $sql =  'SELECT *' . 
+                ' FROM #__enikeishik_ufmexample_items' . 
+                ' WHERE marked!=0' . 
+                ' ORDER BY created_at DESC';
+        return $this->db->getItems($sql);
+    }
+}
